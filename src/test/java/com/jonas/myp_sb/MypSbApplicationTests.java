e71f83198108785b68f66af2b1a83ce16e551f52
JonasChen <tom200425@gmail.com>
2023-03-24 10:16:47 +0800

    創建檔案

---
+++ src/test/java/com/jonas/myp_sb/MypSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonas.myp_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
