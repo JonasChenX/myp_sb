37d91c054636b61fce62512e1ba923716e96f527
JonasChen <tom200425@gamil.com>
2023-04-22 16:57:47 +0800

    串接 綠界API

---
+++ src/main/java/com/jonas/myp_sb/example/EcPay/OrderController.java
@@ -0,0 +1,19 @@
+package com.jonas.myp_sb.example.EcPay;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+
+    @Autowired
+    OrderService orderService;
+
+    @PostMapping("/ecpayCheckout")
+    public String ecpayCheckout() {
+        String aioCheckOutALLForm = orderService.ecpayCheckout();
+
+        return aioCheckOutALLForm;
+    }
+}
